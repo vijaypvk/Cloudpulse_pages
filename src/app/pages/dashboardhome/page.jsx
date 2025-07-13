4c237834c6e96cf11927ea5f53f492a5142dc920
vijaypvk <vijaypvk001@gmail.com>
2025-07-13 20:02:56 +0530

    .

---
+++ src/app/pages/dashboardhome/page.jsx
@@ -0,0 +1,12 @@
+// src/app/pages/dashboardhome/page.jsx
+"use client"; // if you're using client components
+
+import React from 'react';
+
+export default function DashboardHome() {
+  return (
+    <div>
+      <h1>Welcome to Dashboard Home</h1>
+    </div>
+  );
+}
